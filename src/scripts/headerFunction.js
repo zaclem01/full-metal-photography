f0097da5997fb3379d137351e394c7ba54a8cc96
Zac Clemans <zaclem01@gmail.com>
2016-09-21 10:41:15 -0400

    finalize basic header and sub menu layout and function

--- src/scripts/headerFunction.js
+++ src/scripts/headerFunction.js
@@ -1,23 +1,31 @@
 import $ from 'jquery';
 
 export default function() {
-    $('.topBar_menuBtn').click(function(event) {
-        event.stopPropagation();
-        console.log('menu click')
-        $('.sidebarWrapper').addClass('visible');
+    const $sidebar = $('.sidebarWrapper');
+
+    // Open sidebar menu when clicked
+    $('.js-menuBtn').click(function(event) {
+        event.stopPropagation(); // Stop bubbling to upper DOM elements
+        $sidebar.addClass('js-visible');
     });
 
-    // $('.sidebar_closeBtn').click(function() {
-    //     $('.sidebarWrapper').removeClass('visible');
-    // });
-
+    // Close sidebar menu when anywhere but menu is clicked
     $('body > *').not('.sidebarWrapper, .topBar_menuBtn').click(function(event) {
-        event.stopPropagation();
-        console.log($(this))
-        console.log('elsewhere click')
-        if ($('.sidebarWrapper').hasClass('visible')) {
-            console.log('remove')
-            $('.sidebarWrapper').removeClass('visible');
+        event.stopPropagation(); // Stop bubbling to upper DOM elements
+        if ($sidebar.hasClass('js-visible')) {
+            $sidebar.removeClass('js-visible');
         }
-    })
+        // Close the sub menus when sidebar is closed
+        if ($('.js-subMenu').children('.nav_item_subMenu').hasClass('js-visible')) {
+            $('.js-subMenu').click();
+        }
+    });
+
+    // Open submenus
+    $('.js-subMenu').click(function() {
+        const $subMenu = $(this).children('.nav_item_subMenu');
+        $(this).children('a').children('i').toggleClass('fa-flip-vertical');
+        $subMenu.toggleClass('js-hidden');
+        $subMenu.toggleClass('js-visible');
+    });
 }
